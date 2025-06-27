bfb48fdaf544b4aef38579d96e93d084d567d651
Dev Selimcan <selimcangursu@asemsoft.com>
2025-06-27 21:58:45 +0300

    satış noktaları logoları depolama alanına alındı tüm anlaşmalı noktalar logosu listelendi.

--- frontend/src/pages/Home/components/PointOfSale/PointOfSale.jsx
+++ frontend/src/pages/Home/components/PointOfSale/PointOfSale.jsx
@@ -3,52 +3,80 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import "./PointOfSale.css";
 
-// Satış noktası verisi
+import AmazonLogo from '../../../../components/assets/store-list/amazon.webp';
+import BeymenLogo from '../../../../components/assets/store-list/beymen.webp';
+import DrLogo from '../../../../components/assets/store-list/dr.png';
+import HepsiburadaLogo from '../../../../components/assets/store-list/hepsiburada.png';
+import MediaMarktLogo from '../../../../components/assets/store-list/media-markt.png';
+import N11Logo from '../../../../components/assets/store-list/n11.webp';
+import NezihLogo from '../../../../components/assets/store-list/nezih.png';
+import TeknosaLogo from '../../../../components/assets/store-list/teknosa.png';
+import TrendyolLogo from '../../../../components/assets/store-list/trendyol.png';
+import TurkcellLogo from '../../../../components/assets/store-list/turkcell.png';
+import VodofoneLogo from '../../../../components/assets/store-list/vodafone-logo.webp';
+
+// Satış noktası verisi (tüm logo importlarını kullandım)
 const pointOfSaleItems = [
   {
     id: 1,
-    title: "MediaMarkt",
-    image:
-      "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/media-markt.png",
+    title: "Amazon",
+    image: AmazonLogo,
   },
   {
     id: 2,
-    title: "Nezih Kitap",
-    image: "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/nezih.png",
+    title: "Beymen",
+    image: BeymenLogo,
   },
   {
     id: 3,
     title: "D&R",
-    image: "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/dr.png",
+    image: DrLogo,
   },
   {
     id: 4,
-    title: "Amazon Türkiye",
-    image: "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/amazon.png",
+    title: "Hepsiburada",
+    image: HepsiburadaLogo,
   },
   {
     id: 5,
-    title: "N11",
-    image: "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/n11.png",
+    title: "MediaMarkt",
+    image: MediaMarktLogo,
   },
   {
     id: 6,
-    title: "Teknosa",
-    image: "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/teknosa.png",
+    title: "N11",
+    image: N11Logo,
   },
   {
     id: 7,
+    title: "Nezih",
+    image: NezihLogo,
+  },
+  {
+    id: 8,
+    title: "Teknosa",
+    image: TeknosaLogo,
+  },
+  {
+    id: 9,
     title: "Trendyol",
-    image: "https://www.wikywatch.com.tr/wp-content/uploads/2024/01/trendyol.png",
+    image: TrendyolLogo,
+  },
+  {
+    id: 10,
+    title: "Turkcell",
+    image: TurkcellLogo,
+  },
+  {
+    id: 11,
+    title: "Vodafone",
+    image: VodofoneLogo,
   },
 ];
 
 function PointOfSale() {
   return (
-    <section
-      className="pos-slider container-fluid"
-      aria-label="Satış Noktaları"
-    >
+    <section className="pos-slider container-fluid" aria-label="Satış Noktaları">
       <h2 className="pos-title">Wiky Satış Noktaları</h2>
       <Splide
         options={{
